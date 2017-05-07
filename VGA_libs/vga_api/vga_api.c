c5336ccb9e306cca087eb0ee176165d301720cdb
mourix <mourix@live.nl>
2017-05-07 16:06:45 +0200

    Afmaken rechthoek-setpixel-readpixel, toevoegen clearscherm, éénduidigheid commentaar

--- VGA_libs/vga_api/vga_api.c
+++ VGA_libs/vga_api/vga_api.c
@@ -2,7 +2,7 @@
 vga_api.h
 
 
-
+HU Software Ontwikkeling.
 (c) Jos, Tijmen, Niels 05/2017
 ***************************************************************/
 
@@ -37,41 +37,47 @@ void ellips(uint16_t x_mp, uint16_t y_mp, uint16_t radius_x, uint16_t radius_y, 
 }
 
 /***************************
-rechthoek functie
+Rechthoek: teken een rechthoek.
+(x_lo, y_lo, x_rb, y_rb, dikte, kleur, gevuld)
 (c) Jos van Mourik
 ***************************/
 void rechthoek(uint16_t x_lo, uint16_t y_lo, uint16_t x_rb, uint16_t y_rb, uint8_t dikte, uint8_t kleur, bool gevuld)
 {
-	if(gevuld)
+	// Tekenen buiten scherm voorkomen
+	if(x_rb>=VGA_DISPLAY_X) x_rb = VGA_DISPLAY_X-1;
+	if(y_lo>=VGA_DISPLAY_Y) y_lo = VGA_DISPLAY_Y-1;
+
+	// Gevulde rechthoek of overflow lijndikte
+	if(gevuld || dikte >= ((x_rb-x_lo)/2) || dikte >= ((y_lo-y_rb)/2))
 	{
-		// Verticale lijnen vullen
-		for (uint16_t j = y_rb; j < (y_lo+1); j++)
+		// Verticale coördinaten
+		for (uint16_t y = y_rb; y < (y_lo+1); y++)
 		{
-			// Horizontale lijnen vullen
-			for(uint16_t i = x_lo; i < (x_rb+1); i++)
+			// Horizontale coördinaten
+			for(uint16_t x = x_lo; x < (x_rb+1); x++)
 			{
-				  setpixel(i, j, kleur);
+				setpixel(x, y, kleur);
 			}
 		}
 	}
 
 	else
 	{
-		// stappen door lijndikte
-		for(uint8_t k = 0; k < dikte; k++)
+		// Teken lijndikte
+		for(uint8_t d = 0; d < dikte; d++)
 		{
-			// Teken horizontale lijnen
-			for(uint16_t i = x_lo; i < (x_rb+1); i++)
+			// Teken horizontale lijnen van rechthoek
+			for(uint16_t x = x_lo; x < (x_rb+1); x++)
 			{
-				  setpixel(i, (y_rb+k), kleur);
-				  setpixel(i, (y_lo-k), kleur);
+				setpixel(x, (y_rb+d), kleur);
+				setpixel(x, (y_lo-d), kleur);
 			}
 
-			//Teken verticale lijnen
-			for(uint16_t j = (y_rb+dikte); j < y_lo+1-dikte; j++)
+			// Teken verticale lijnen van rechthoek
+			for(uint16_t y = (y_rb+dikte); y < (y_lo+1-dikte); y++)
 			{
-				  setpixel((x_lo+k), j, kleur);
-				  setpixel((x_rb-k), j, kleur);
+				setpixel((x_lo+d), y, kleur);
+				setpixel((x_rb-d), y, kleur);
 			}
 		}
 	}
@@ -86,24 +92,24 @@ void driehoek(uint16_t x_1, uint16_t y_1, uint16_t x_2, uint16_t y_2, uint16_t x
 }
 
 /***************************
-setpixel functie: verandert één pixel van kleur.
-(c) Jos van Mourik
+Setpixel: verandert één pixel van kleur.
+(x, y, kleur)
+(c) Franc van der Bent, aangepast door Jos van Mourik
 ***************************/
 void setpixel(uint16_t x, uint16_t y, uint8_t kleur)
 {
-	if((x<=VGA_DISPLAY_X) && (y<=VGA_DISPLAY_Y))
-	{
-		VGA_RAM1[(y*(VGA_DISPLAY_X+1))+x] = kleur;
-	}
+	if((x<=VGA_DISPLAY_X) && (y<=VGA_DISPLAY_Y)) VGA_RAM1[(y*(VGA_DISPLAY_X+1))+x] = kleur;
 }
 
 /***************************
-readpixel functie: geeft de waarde van één pixel terug.
+Readpixel: geef de waarde van één pixel terug.
+(x, y)
 (c) Jos van Mourik
 ***************************/
 uint8_t readpixel(uint16_t x, uint16_t y)
 {
-	return VGA_RAM1[(y*(VGA_DISPLAY_X+1))+x];
+	if((x<=VGA_DISPLAY_X) && (y<=VGA_DISPLAY_Y)) return VGA_RAM1[(y*(VGA_DISPLAY_X+1))+x];
+	else return 0;
 }
 
 /***************************
@@ -115,11 +121,19 @@ void bitmap(uint16_t x_lo, uint16_t y_lo, uint8_t map)
 }
 
 /***************************
-
+Clearscherm: vul het scherm met aangegeven kleur.
+(kleur)
+(c) Franc van der Bent, aangepast door Jos van Mourik
 ***************************/
 void clearscherm(uint8_t kleur)
 {
-
+	for(uint16_t y = 0; y < VGA_DISPLAY_Y; y++)
+	{
+		for(uint16_t x = 0; x < VGA_DISPLAY_X; x++)
+		{
+			setpixel(x, y, kleur);
+		}
+	}
 }
 
 /***************************
