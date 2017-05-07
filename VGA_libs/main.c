d77a05f99a3f96e2d05c265a6085967f24547073
mourix <mourix@live.nl>
2017-05-07 16:03:04 +0200

    Demo: clearscherm functie, meer commentaar, verbeteringen

--- VGA_libs/main.c
+++ VGA_libs/main.c
@@ -18,7 +18,7 @@ int main(void)
 	SystemInit(); // System speed to 168MHz
 
 	UB_VGA_Screen_Init(); // Init VGA-Screen
-	UB_VGA_FillScreen(VGA_COL_BLACK);
+	clearscherm(VGA_COL_BLACK);
 
 	  uint16_t x_lo = 100;
 	  uint16_t y_lo = 200;
@@ -28,8 +28,8 @@ int main(void)
 	  uint8_t rechthoek_kleur = VGA_COL_BLUE;
 	  bool gevuld = 0;
 
-	  //Teken rechthoek
-	  //rechthoek(x_lo, y_lo, x_rb, y_rb, rechthoek_dikte, rechthoek_kleur, gevuld);
+	  // Teken rechthoek
+	  // rechthoek(x_lo, y_lo, x_rb, y_rb, rechthoek_dikte, rechthoek_kleur, gevuld);
 
 
 
@@ -40,24 +40,28 @@ int main(void)
 	  uint8_t lijn_kleur = 70;
 	  uint8_t lijn_dikte = 1;
 
-	  //Teken lijn
-	  //lijn(x_l, y_l, x_r, y_r, lijn_dikte, lijn_kleur);
+	  // Teken lijn
+	  // lijn(x_l, y_l, x_r, y_r, lijn_dikte, lijn_kleur);
 
   while(1)
   {
-	  //Rechthoek demo
-	  for(int i = 0; i <240; i++)
-	  {
-		  UB_VGA_FillScreen(VGA_COL_BLACK);
-		  rechthoek(0, 239, i, (239-i), 1, VGA_COL_BLUE, 0);
-		  for(int j = 0; j <100000; j++);
-	  }
-	  for (int k = 0; k <120; k++)
-	  {
-		  rechthoek(0, 239, 239, 0, k, VGA_COL_BLUE, 0);
-		  for(int j = 0; j <(100000/2); j++);
-	  }
+	  	  // Rechthoek coördinaten demo
+	  	  for(int i = 0; i <240; i++)
+	  	  {
+	  		  clearscherm(VGA_COL_BLACK);
+	  		  rechthoek(0, 239, i, (239-i), 5, VGA_COL_BLUE, 0);
+	  		  for(int j = 0; j <100000; j++);
+	  	  }
 
+	  	  // Rechthoek dikte demo
+	  	  for (int k = 0; k <76; k++)
+	  	  {
+	  		  rechthoek(50, 200, 200, 50, k, VGA_COL_CYAN, 0);
+	  		  for(int j = 0; j <(100000); j++);
+	  	  }
+
+	  	  // Beeld vasthouden
+	  	  for(int j = 0; j <(100000000); j++);
   }
 }
 
