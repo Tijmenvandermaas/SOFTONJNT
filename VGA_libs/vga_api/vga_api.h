22650c4bc191387551c902cb60dde59fe2a0bfd8
Jos <mourix@live.nl>
2017-05-10 19:36:11 +0200

    verbeter bitmap header

--- VGA_libs/vga_api/vga_api.h
+++ VGA_libs/vga_api/vga_api.h
@@ -22,6 +22,6 @@ void rechthoek(uint16_t x_lo, uint16_t y_lo, uint16_t x_rb, uint16_t y_rb, uint8
 void driehoek(uint16_t x_1, uint16_t y_1, uint16_t x_2, uint16_t y_2, uint16_t x_3, uint16_t y_3, uint8_t dikte, uint8_t kleur, bool gevuld);
 void setpixel(uint16_t x, uint16_t y, uint8_t kleur);
 uint8_t readpixel(uint16_t x, uint16_t y);
-void bitmap(uint16_t x_lo, uint16_t y_lo, uint8_t map);
+void bitmap(uint16_t x_lo, uint16_t y_lo, char map[]);
 void clearscherm(uint8_t kleur);
 void wacht(uint16_t msecs);
