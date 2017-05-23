3052dadc3a3dcb8c2c3c4ff63b49eecd61534692
Jos <mourix@live.nl>
2017-05-23 12:54:24 +0200

    Update demo's: rechthoek, lijn, bitmap, kleur

--- VGA_libs/main.c
+++ VGA_libs/main.c
@@ -21,28 +21,25 @@ int main(void)
 	clearscherm(VGA_COL_BLACK);
 
 /*
-	  uint16_t x_lo = 100;
-	  uint16_t y_lo = 200;
-	  uint16_t x_rb = 200;
-	  uint16_t y_rb = 100;
-	  uint8_t rechthoek_dikte = 5;
-	  uint8_t rechthoek_kleur = VGA_COL_BLUE;
-	  bool gevuld = 0;
-
-	  // Teken rechthoek
-	  rechthoek(x_lo, y_lo, x_rb, y_rb, rechthoek_dikte, rechthoek_kleur, gevuld);
-
-	  uint16_t x_l = 50;
-	  uint16_t y_l = 50;
-	  uint16_t x_r = 100;
-	  uint16_t y_r = 100;
-	  uint8_t lijn_kleur = 70;
-	  uint8_t lijn_dikte = 1;
-
-	  // Teken lijn
-	  lijn(x_l, y_l, x_r, y_r, lijn_dikte, lijn_kleur);
+	// Rechthoek demo
+	rechthoek(0, 239, 319, 0, 10, VGA_COL_WHITE, 0);
+	rechthoek(20, 225, 300, 20, 3, VGA_COL_RED, 0);
+	rechthoek(25, 220, 100, 25, 1, VGA_COL_BLUE, 1);
+	rechthoek(150, 130, 250, 30, 1, VGA_COL_GREEN, 1);
+	rechthoek(175, 200, 200, 175, 2, VGA_COL_MAGENTA, 0);
 */
 
+/*
+	  // Lijn demo
+	  lijn(0, 10, 200, 10, 10, VGA_COL_BLUE);
+	  lijn(0, 30, 100, 50, 5, VGA_COL_RED);
+	  lijn(0, 100, 200, 50, 1, VGA_COL_GREEN);
+	  lijn(50, 200, 100, 100, 20, VGA_COL_YELLOW);
+	  lijn(220, 10, 220, 220, 1, VGA_COL_WHITE);
+*/
+
+
+/*
 	  // Bitmap demo
 	  bitmap(25, 100, &emoji_blij_64);
 	  bitmap(100, 100, &emoji_boos_64);
@@ -51,6 +48,25 @@ int main(void)
 	  bitmap(100, 200, &pijl_omlaag_60);
 	  bitmap(175, 200, &pijl_links_60);
 	  bitmap(250, 200, &pijl_rechts_60);
+*/
+
+	/*
+	// Kleuren demo
+	lijn(0,10,239,10, 5, BLAUW);
+	lijn(0,20,239,20, 5, LICHTBLAUW);
+	lijn(0,30,239,30, 5, GROEN);
+	lijn(0,40,239,40, 5, LICHTGROEN);
+	lijn(0,50,239,50, 5, ROOD);
+	lijn(0,60,239,60, 5, LICHTROOD);
+	lijn(0,70,239,70, 5, CYAAN);
+	lijn(0,80,239,80, 5, LICHTCYAAN);
+	lijn(0,90,239,90, 5, MAGENTA);
+	lijn(0,100,239,100, 5, LICHTMAGENTA);
+	lijn(0,110,239,110, 5, BRUIN);
+	lijn(0,120,239,120, 5, GEEL);
+	lijn(0,130,239,130, 5, GRIJS);
+	lijn(0,140,239,140, 5, WIT);
+*/
 
   while(1)
   {
