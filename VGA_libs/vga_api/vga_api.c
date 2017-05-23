f0d8161c47b6172b9e0fa3d0b0f358827891059e
HuNiels <niels.vanrijn@hotmail.com>
2017-05-23 11:50:39 +0200

    Begin text en driehoek functie

--- VGA_libs/vga_api/vga_api.c
+++ VGA_libs/vga_api/vga_api.c
@@ -15,9 +15,23 @@ Includes
 /***************************
 
 ***************************/
-void tekst(uint16_t x, uint16_t y, uint8_t tekst[100], uint8_t font,  uint8_t grootte,  uint8_t kleur,  uint8_t stijl)
+//void tekst(uint16_t x, uint16_t y, uint8_t tekst[100], uint8_t font,  uint8_t grootte,  uint8_t kleur,  uint8_t stijl)
+void tekst(uint16_t x_lo, uint16_t y_lo, char map[], uint8_t letter)
 {
+	// Start bitmap data in array
+	uint16_t cursor = letter * 12 + 4;
 
+	// Bereken startpunt ten opzichte van linksonder
+	for(uint16_t y = (y_lo-map[2]-1); y < y_lo-1; y++)
+	{
+		// Stap door bytes
+		for(uint16_t x = x_lo; x < (x_lo+map[0]); x+=8)
+		{
+			// Stap door bits en plaats witte pixel als bit hoog is
+			for(uint16_t bit = 0; bit <8; bit++) {if(map[cursor] & (1 << (7-bit))){ setpixel(x+bit, y, VGA_COL_WHITE);}}
+			cursor++;
+		}
+	}
 }
 
 /***************************
@@ -270,9 +284,11 @@ void rechthoek(uint16_t x_lo, uint16_t y_lo, uint16_t x_rb, uint16_t y_rb, uint8
 /***************************
 
 ***************************/
-void driehoek(uint16_t x_1, uint16_t y_1, uint16_t x_2, uint16_t y_2, uint16_t x_3, uint16_t y_3, uint8_t dikte, uint8_t kleur, bool gevuld)
+void driehoek(uint16_t x_1, uint16_t y_1, uint16_t x_2, uint16_t y_2, uint16_t x_3, uint16_t y_3, uint8_t dikte, uint8_t kleur)
 {
-
+	lijn(x_1,y_1,x_2,y_2,dikte,kleur); //Print de drie lijnen waaruit de driehoek bestaat.
+	lijn(x_1,y_1,x_3,y_3,dikte,kleur);
+	lijn(x_2,y_2,x_3,y_3,dikte,kleur);
 }
 
 /***************************
